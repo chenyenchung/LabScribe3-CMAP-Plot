2caf7145b60ba4475b01d1e45d5880281f2f0029
egoweaver <chenyenjung@gmail.com>
2017-09-26 11:37:33 +0800

    # Detailed plotting is added (Overwhelming)

--- CMAP_graph_maker.R
+++ CMAP_graph_maker.R
@@ -23,19 +23,64 @@ rawTable <- tbl_df(rawTable)
 rawTable <- group_by(rawTable, Type, Age, ROI, fNum)
 sumTable <- summarise(rawTable, mV = mean(EMG), sd = sd(EMG))
 sumTable$ROI <- factor(paste(sumTable$ROI, "mA"))
-sumTable$Age <- paste(AgeUnit, sumTable$Age)
+sumTable$Age <- factor(paste(AgeUnit, sumTable$Age))
 
 # Draw an overview graph
-pdf(paste0("./CMAP_Overview_",Sys.time(),".pdf"), paper = "a4r")
+if(!dir.exists("./Graph")){
+  dir.create("./Graph")
+}
+
+pdf(paste0("./Graph/CMAP_Overview_",Sys.time(),".pdf"), paper = "a4r")
 print(ggplot(sumTable, aes(x = fNum*2, y = mV)) + 
   geom_line(aes(colour = Type)) +
   facet_grid(ROI ~ Age) +
   scale_color_discrete(name = "Mouse Identity") +
   xlab("Time (ms)") +
   ylab("Compound Muscle Action Potential (mV)") +
+  ggtitle("Summary") +
   theme_minimal())
 dev.off()
 
 sapply(levels(sumTable$ROI), function(x){
-  
+  pdf(paste0("./Graph/CMAP_",x,"_",Sys.time(),".pdf"), paper = "a4r")
+  print(ggplot(filter(sumTable, ROI == x), aes(x = fNum*2, y = mV)) + 
+          geom_line(aes(colour = Age)) +
+          facet_grid(Type ~ .) +
+          scale_color_discrete(name = "Age When Assayed") +
+          xlab("Time (ms)") +
+          ylab("Compound Muscle Action Potential (mV)") +
+          ggtitle(x) +
+          theme_minimal())
+  dev.off()
 })
+
+sapply(levels(sumTable$Age), function(x){
+  pdf(paste0("./Graph/CMAP_mA_on_Type_", x,"_",Sys.time(),".pdf"), paper = "a4r")
+  print(ggplot(filter(sumTable, Age == x), aes(x = fNum*2, y = mV)) + 
+          geom_line(aes(colour = ROI)) +
+          facet_grid(Type ~ .) +
+          scale_color_discrete(name = "Stimulation (mA)") +
+          xlab("Time (ms)") +
+          ylab("Compound Muscle Action Potential (mV)") +
+          ggtitle(x) +
+          theme_minimal())
+  dev.off()
+  sapply(levels(sumTable$ROI), function(y){
+    pdf(paste0("./Graph/CMAP_Type_on_mA",y,"_",x,"_",Sys.time(),".pdf"), paper = "a4r")
+    print(ggplot(filter(sumTable, ROI == y), aes(x = fNum*2, y = mV)) + 
+            geom_line(aes(colour = Type)) +
+            scale_color_discrete(name = "Mouse Identity") +
+            xlab("Time (ms)") +
+            ylab("Compound Muscle Action Potential (mV)") +
+            ggtitle(paste(y,"on",x)) +
+            theme_minimal())
+    dev.off()
+  })
+
+})
+
+if(!file.exists("~/Desktop/CMAP_data/summary.csv")){
+  write.csv(sumTable, file = "~/Desktop/CMAP_data/summary.csv")
+} else {
+  write.csv(sumTable, file = paste0("~/Desktop/CMAP_data/summary_", Sys.time(), ".csv"))
+}
